4a6c10a273f4a24f2ab4a06557efa4d35ba8b2f7
aarizaf <aarizaf@uninorte.edu.co>
2026-07-27 12:45:16 -0500

    ruta endpoint

--- routes/results.py
+++ routes/results.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from models.result import ObtenerGrupos
+from models.result import ObtenerInfo
 from services import result_service
 from typing import List
 from fastapi.responses import JSONResponse
@@ -10,6 +10,6 @@ router = APIRouter(prefix="/SaberResults", tags=["Results"])
 
 
 @router.post("/obtenerInfoclte")
-def obtener_infoclte(body: ObtenerGrupos):
+def obtener_infoclte(body: ObtenerInfo):
     return result_service.obtener_infoclte(body)
 
